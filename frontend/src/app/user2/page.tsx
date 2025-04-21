3f5065e94ea3269fc9b3d8e4aa981703dad72d85
jazilhashmi <jazilhashmi2017@gmail.com>
2025-04-21 20:26:32 +0500

    made user2 page

---
+++ frontend/src/app/user2/page.tsx
@@ -0,0 +1,19 @@
+
+// async function getHomePageData() {
+//   const res = await fetch("http://localhost:1337/api/home-pages");
+//   const data = await res.json();
+//   return data.data?.[0];
+// }
+
+
+export default async function User1Page() {
+  // const data = await getHomePageData();
+  // console.log("Fetched data:", data);
+
+
+  return (
+    <div>
+        USER2
+    </div>
+  );
+}
